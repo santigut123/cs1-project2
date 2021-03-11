9da35989e56f3309be79eb543abcfcbbea8b6e95
Santiago Gutierrez <santigut@tutamail.com>
2021-03-11 09:39:04 -0500

    Course 1 functionality finished, course 2 left

--- main.c
+++ main.c
@@ -54,6 +54,10 @@ failFish* create_failfish(int num){
     return new;
 
 }
+void dispose_failfish(failFish* f){
+    free(f);
+
+}
 failfish_queue* create_failfish_queue(char *pondname,int pondNum,int n, int e, int th){
     failfish_queue* q= malloc(sizeof(failfish_queue));
     q->pondNum=pondNum;
@@ -61,6 +65,8 @@ failfish_queue* create_failfish_queue(char *pondname,int pondNum,int n, int e, i
     q->n=n;
     q->e=e;
     q->th=th;
+    q->head=NULL;
+    q->tail=NULL;
     return q;
 
 }
@@ -78,9 +84,12 @@ void dispose_failfish_queues(failfish_queue* ponds[11]){
 void print_failfish_queue(failfish_queue* q){
     printf("%d ",q->pondNum);
     printf("%s ",q->name);
-    printf("%d ", q->n);
-    printf("%d ",q->e);
-    printf("%d \n",q->th);
+    failFish* currentFish=q->tail;
+    for(int i=0;i<q->n;i++){
+        printf("%d ",currentFish->num);
+        currentFish=currentFish->prev;
+    }
+    printf("\n");
 }
 int get_num_ponds(char* buf){
 
@@ -141,9 +150,7 @@ void remove_failfish(failfish_queue *q,failFish* f){
         q->tail->next = q->head;
         return;
     }
-
     /* Case 3: Removing the tail. */
-
     if(f == q->tail) {
         q->tail = q->tail->prev;
         q->tail->next = q->head;
@@ -170,7 +177,7 @@ failFish* getfailfish(failfish_queue *q,int num){
 }
 void fill_pond(failfish_queue* pond){
     for(int i=0;i<pond->n;i++){
-        add_failfish(create_failfish(i+1));
+        add_failfish(pond,create_failfish(i+1));
     }
 }
 void fill_ponds(failfish_queue* ponds[11]){
@@ -179,6 +186,94 @@ void fill_ponds(failfish_queue* ponds[11]){
             fill_pond(ponds[i]);
         }
     }
+}
+
+/* Dispose of a list of monsters and all the monsters in it.
+
+   This is an O(n) operation that can't be improved (we have to visit each monster
+   to dispose it). */
+
+void dispose_all_failfish(failfish_queue *q)
+{
+    failFish *m = q->head;
+    failFish *next;
+
+    /* First, check if the list is empty. */
+
+    if(m == NULL)
+    {
+        dispose_failfish_queue(q);
+        return;
+    }
+
+    /* Okay, now we know the list isn't empty, so we can free at least one element. */
+
+    do {
+        /* We can't use m->next after disposing of m, so we save a copy of m->next and
+           then copy it back after disposing m. */
+
+        next = m->next;
+        dispose_failfish(m);
+        m = next;
+    } while(m != q->head);
+
+    /* ml->head - and therefore m - are dangling pointers here!  But that's fine as long
+       as we don't dereference them. */
+
+    dispose_failfish_queue(q);
+}
+void print_failfish_queues(failfish_queue* ponds[11]){
+    for(int i = 1;i<11;i++){
+        if(ponds[i]!=NULL){
+            print_failfish_queue(ponds[i]);
+        }
+    }
+
+}
+void dispose_all_ponds(failfish_queue* ponds[11]){
+    for(int i =1;i<11;i++){
+        if(ponds[i]!=NULL){
+            dispose_all_failfish(ponds[i]);
+        }
+    }
+
+}
+//removes failfish and returns pointer to the next failfish
+failFish* remove_fish_with_distance(failfish_queue* q, failFish* f,int e){
+    failFish* nextFish=f;
+    failFish* reference;
+    for(int i=0;i<e-1;i++){
+        nextFish=nextFish->prev;
+    }
+    reference=nextFish->prev;
+    printf("FailFish %d Eaten \n",nextFish->num);
+    remove_failfish(q, nextFish);
+    dispose_failfish(nextFish);
+    q->n--;
+    return reference;
+
+
+}
+void pond_first_course(failfish_queue* q){
+    printf("Pond %d: %s \n",q->pondNum,q->name);
+    failFish* startingFish=q->tail;
+    failFish* newFish;
+    printf("starting tail number %d \n",startingFish->num);
+    while(q->n > q->th){
+        newFish= remove_fish_with_distance(q,startingFish,q->e);
+        startingFish=newFish;
+    }
+}
+void ponds_first_course(failfish_queue* ponds[11]){
+    printf("First Course \n \n");
+    for(int i=1;i<11;i++){
+        if(ponds[i]!=NULL){
+            pond_first_course(ponds[i]);
+            printf("\n");
+        }
+
+    }
+
 }
 int main(void){
     int numberPonds;
@@ -192,11 +287,19 @@ int main(void){
     }
     //get pond information from file
     get_ponds_info(ponds, buf, ifp);
-    for(int i = 1;i<11;i++){
-        if(ponds[i]!=NULL){
-            print_failfish_queue(ponds[i]);
-        }
-    }
+    //fill ponds with appropriate amount of fish
+    fill_ponds(ponds);
+    //print each pond
+    print_failfish_queues(ponds);
+    ponds_first_course(ponds);
+    printf("End of Course Pond Status \n");
+    print_failfish_queues(ponds);
+    //dispose all the ponds
+    dispose_all_ponds(ponds);
+
+
+    
+
 
 
 
